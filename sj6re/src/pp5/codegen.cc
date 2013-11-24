6d8e83637c94133f719b7c4a0006874771c73d28
sj6re <sj6re@labunix01.cs.virginia.edu>
2013-11-23 20:54:37 -0500

    working version 1

--- sj6re/src/pp5/codegen.cc
+++ sj6re/src/pp5/codegen.cc
@@ -4,18 +4,20 @@
  * too fancy, mostly just create objects of the various Tac instruction
  * classes and append them to the list.
  */
-
+ 
 #include "codegen.h"
 #include <string.h>
 #include "tac.h"
 #include "mips.h"
-
-Location* CodeGenerator::ThisPtr= new Location(fpRelative, 4, "this");
-  
+#include "errors.h"
+ 
 CodeGenerator::CodeGenerator()
 {
+  code = new List<Instruction*>();
+  localOffset = OffsetToFirstLocal;
+  mainDefined = false;
 }
-
+ 
 char *CodeGenerator::NewLabel()
 {
   static int nextLabelNum = 0;
@@ -23,132 +25,138 @@ char *CodeGenerator::NewLabel()
   sprintf(temp, "_L%d", nextLabelNum++);
   return strdup(temp);
 }
-
-
+ 
 Location *CodeGenerator::GenTempVar()
 {
   static int nextTempNum;
   char temp[10];
   Location *result = NULL;
   sprintf(temp, "_tmp%d", nextTempNum++);
-  /* pp5: need to create variable in proper location
+  /* pp4: need to create variable in proper location
      in stack frame for use as temporary. Until you
      do that, the assert below will always fail to remind
      you this needs to be implemented  */
+  result = new Location(fpRelative, localOffset, temp);
+  localOffset -= VarSize;
+ 
   Assert(result != NULL);
   return result;
 }
-
+ 
+Location *CodeGenerator::GenLocalVar(const char *name, int size)
+{
+    Location *result = new Location(fpRelative, localOffset, name);
+    localOffset -= size;
+    return result;
+}
  
 Location *CodeGenerator::GenLoadConstant(int value)
 {
   Location *result = GenTempVar();
-  code.push_back(new LoadConstant(result, value));
+  code->Append(new LoadConstant(result, value));
   return result;
 }
-
+ 
 Location *CodeGenerator::GenLoadConstant(const char *s)
 {
   Location *result = GenTempVar();
-  code.push_back(new LoadStringConstant(result, s));
+  code->Append(new LoadStringConstant(result, s));
   return result;
-} 
-
+}
+ 
 Location *CodeGenerator::GenLoadLabel(const char *label)
 {
   Location *result = GenTempVar();
-  code.push_back(new LoadLabel(result, label));
+  code->Append(new LoadLabel(result, label));
   return result;
-} 
-
-
+}
+ 
 void CodeGenerator::GenAssign(Location *dst, Location *src)
 {
-  code.push_back(new Assign(dst, src));
+  code->Append(new Assign(dst, src));
 }
-
-
+ 
 Location *CodeGenerator::GenLoad(Location *ref, int offset)
 {
   Location *result = GenTempVar();
-  code.push_back(new Load(result, ref, offset));
+  code->Append(new Load(result, ref, offset));
   return result;
 }
-
+ 
 void CodeGenerator::GenStore(Location *dst,Location *src, int offset)
 {
-  code.push_back(new Store(dst, src, offset));
+  code->Append(new Store(dst, src, offset));
 }
-
-
+ 
 Location *CodeGenerator::GenBinaryOp(const char *opName, Location *op1,
-						     Location *op2)
+                             Location *op2)
 {
   Location *result = GenTempVar();
-  code.push_back(new BinaryOp(BinaryOp::OpCodeForName(opName), result, op1, op2));
+  code->Append(new BinaryOp(BinaryOp::OpCodeForName(opName), result, op1, op2));
   return result;
 }
-
-
+ 
 void CodeGenerator::GenLabel(const char *label)
 {
-  code.push_back(new Label(label));
+  if (strcmp(label, "main") == 0)
+    mainDefined = true;
+ 
+  code->Append(new Label(label));
 }
-
+ 
 void CodeGenerator::GenIfZ(Location *test, const char *label)
 {
-  code.push_back(new IfZ(test, label));
+  code->Append(new IfZ(test, label));
 }
-
+ 
 void CodeGenerator::GenGoto(const char *label)
 {
-  code.push_back(new Goto(label));
+  code->Append(new Goto(label));
 }
-
+ 
 void CodeGenerator::GenReturn(Location *val)
 {
-  code.push_back(new Return(val));
+  code->Append(new Return(val));
 }
-
-
+ 
 BeginFunc *CodeGenerator::GenBeginFunc()
 {
+  localOffset = OffsetToFirstLocal;
   BeginFunc *result = new BeginFunc;
-  code.push_back(result);
+  code->Append(result);
   return result;
 }
-
+ 
 void CodeGenerator::GenEndFunc()
 {
-  code.push_back(new EndFunc());
+  code->Append(new EndFunc());
 }
-
+ 
 void CodeGenerator::GenPushParam(Location *param)
 {
-  code.push_back(new PushParam(param));
+  code->Append(new PushParam(param));
 }
-
+ 
 void CodeGenerator::GenPopParams(int numBytesOfParams)
 {
   Assert(numBytesOfParams >= 0 && numBytesOfParams % VarSize == 0); // sanity check
   if (numBytesOfParams > 0)
-    code.push_back(new PopParams(numBytesOfParams));
+    code->Append(new PopParams(numBytesOfParams));
 }
-
+ 
 Location *CodeGenerator::GenLCall(const char *label, bool fnHasReturnValue)
 {
   Location *result = fnHasReturnValue ? GenTempVar() : NULL;
-  code.push_back(new LCall(label, result));
-  return result;
-}
-
-Location *CodeGenerator::GenACall(Location *fnAddr, bool fnHasReturnValue)
-{
-  Location *result = fnHasReturnValue ? GenTempVar() : NULL;
-  code.push_back(new ACall(fnAddr, result));
+  code->Append(new LCall(label, result));
   return result;
 }
  
+Location *CodeGenerator::GenACall(Location *fnAddr, bool fnHasReturnValue)
+{
+  Location *result = fnHasReturnValue ? GenTempVar() : NULL;
+  code->Append(new ACall(fnAddr, result));
+  return result;
+}
  
 static struct _builtin {
   const char *label;
@@ -163,48 +171,46 @@ static struct _builtin {
   {"_PrintString", 1, false},
   {"_PrintBool", 1, false},
   {"_Halt", 0, false}};
-
+ 
 Location *CodeGenerator::GenBuiltInCall(BuiltIn bn,Location *arg1, Location *arg2)
 {
   Assert(bn >= 0 && bn < NumBuiltIns);
   struct _builtin *b = &builtins[bn];
   Location *result = NULL;
-
+ 
   if (b->hasReturn) result = GenTempVar();
                 // verify appropriate number of non-NULL arguments given
   Assert((b->numArgs == 0 && !arg1 && !arg2)
-	|| (b->numArgs == 1 && arg1 && !arg2)
-	|| (b->numArgs == 2 && arg1 && arg2));
-  if (arg2) code.push_back(new PushParam(arg2));
-  if (arg1) code.push_back(new PushParam(arg1));
-  code.push_back(new LCall(b->label, result));
+    || (b->numArgs == 1 && arg1 && !arg2)
+    || (b->numArgs == 2 && arg1 && arg2));
+  if (arg2) code->Append(new PushParam(arg2));
+  if (arg1) code->Append(new PushParam(arg1));
+  code->Append(new LCall(b->label, result));
   GenPopParams(VarSize*b->numArgs);
   return result;
 }
-
-
+ 
 void CodeGenerator::GenVTable(const char *className, List<const char *> *methodLabels)
 {
-  code.push_back(new VTable(className, methodLabels));
+  code->Append(new VTable(className, methodLabels));
 }
-
-
+ 
 void CodeGenerator::DoFinalCodeGen()
 {
+  if (!mainDefined)
+    ReportError::NoMainFound();
+ 
   if (IsDebugOn("tac")) { // if debug don't translate to mips, just print Tac
-    std::list<Instruction*>::iterator p;
-    for (p= code.begin(); p != code.end(); ++p) {
-      (*p)->Print();
-    }
+    for (int i = 0; i < code->NumElements(); i++)
+    code->Nth(i)->Print();
    }  else {
      Mips mips;
      mips.EmitPreamble();
-
-    std::list<Instruction*>::iterator p;
-    for (p= code.begin(); p != code.end(); ++p) {
-      (*p)->Emit(&mips);
-    }
+     for (int i = 0; i < code->NumElements(); i++)
+     code->Nth(i)->Emit(&mips);
   }
 }
+
+
 
 
--- sj6re/src/pp5/codegen.h
+++ sj6re/src/pp5/codegen.h
@@ -11,22 +11,25 @@
  *          dispatch, array length(), etc.)
  */
 
+
 #ifndef _H_codegen
 #define _H_codegen
-
-#include <cstdlib>
-#include <list>
+ 
+#include <stdlib.h>
+#include "list.h"
 #include "tac.h"
  
-
               // These codes are used to identify the built-in functions
 typedef enum { Alloc, ReadLine, ReadInteger, StringEqual,
                PrintInt, PrintString, PrintBool, Halt, NumBuiltIns } BuiltIn;
-
+ 
 class CodeGenerator {
   private:
-    std::list<Instruction*> code;
-
+    List<Instruction*> *code;
+ 
+    int localOffset;
+    bool mainDefined;
+ 
   public:
            // Here are some class constants to remind you of the offsets
            // used for globals, locals, and parameters. You will be
@@ -40,24 +43,24 @@ class CodeGenerator {
            // Conveniently, all vars are 4 bytes in size for code generation
     static const int OffsetToFirstLocal = -8,
                      OffsetToFirstParam = 4,
+                     OffsetToFirstField = 4,
+                     OffsetToFirstMethod = 0,
                      OffsetToFirstGlobal = 0;
     static const int VarSize = 4;
-
-    static Location* ThisPtr;
-
+ 
     CodeGenerator();
-    
+ 
          // Assigns a new unique label name and returns it. Does not
          // generate any Tac instructions (see GenLabel below if needed)
     char *NewLabel();
-
-    
+ 
          // Creates and returns a Location for a new uniquely named
          // temp variable. Does not generate any Tac instructions
     Location *GenTempVar();
-
+    Location *GenLocalVar(const char *name, int size);
+ 
          // Generates Tac instructions to load a constant value. Creates
-         // a new temp var to hold the result. The constant 
+         // a new temp var to hold the result. The constant
          // value is passed as an integer, it can be 0 for integer zero,
          // false for bool, NULL for null object, etc. All are just 4-byte
          // zero in the code generation world.
@@ -68,18 +71,17 @@ class CodeGenerator {
     Location *GenLoadConstant(int value);
     Location *GenLoadConstant(const char *str);
     Location *GenLoadLabel(const char *label);
-
-
+ 
          // Generates Tac instructions to copy value from one location to another
     void GenAssign(Location *dst, Location *src);
-
+ 
          // Generates Tac instructions to dereference addr and store value
          // into that memory location. addr should hold a valid memory address
          // (most likely computed from an array or field offset calculation).
          // The optional offset argument can be used to offset the addr by a
          // positive/negative number of bytes. If not given, 0 is assumed.
     void GenStore(Location *addr, Location *val, int offset = 0);
-
+ 
          // Generates Tac instructions to dereference addr and load contents
          // from a memory location into a new temp var. addr should hold a
          // valid memory address (most likely computed from an array or
@@ -88,53 +90,50 @@ class CodeGenerator {
          // offset argument can be used to offset the addr by a positive or
          // negative number of bytes. If not given, 0 is assumed.
     Location *GenLoad(Location *addr, int offset = 0);
-
-    
+ 
          // Generates Tac instructions to perform one of the binary ops
          // identified by string name, such as "+" or "==".  Returns a
          // Location object for the new temporary where the result
          // was stored.
     Location *GenBinaryOp(const char *opName, Location *op1, Location *op2);
-
-    
+ 
          // Generates the Tac instruction for pushing a single
          // parameter. Used to set up for ACall and LCall instructions.
          // The Decaf convention is that parameters are pushed right
          // to left (so the first argument is pushed last)
     void GenPushParam(Location *param);
-
+ 
          // Generates the Tac instruction for popping parameters to
          // clean up after an ACall or LCall instruction. All parameters
          // are removed with one adjustment of the stack pointer.
     void GenPopParams(int numBytesOfParams);
-
+ 
          // Generates the Tac instructions for a LCall, a jump to
          // a compile-time label. The params to the target routine
          // should already have been pushed. If hasReturnValue is
-         // true,  a new temp var is created, the fn result is stored 
+         // true,  a new temp var is created, the fn result is stored
          // there and that Location is returned. If false, no temp is
          // created and NULL is returned
     Location *GenLCall(const char *label, bool fnHasReturnValue);
-
+ 
          // Generates the Tac instructions for ACall, a jump to an
          // address computed at runtime. Works similarly to LCall,
          // described above, in terms of return type.
          // The fnAddr Location is expected to hold the address of
          // the code to jump to (typically it was read from the vtable)
     Location *GenACall(Location *fnAddr, bool fnHasReturnValue);
-
+ 
          // Generates the Tac instructions to call one of
          // the built-in functions (Read, Print, Alloc, etc.) Although
          // you could just make a call to GenLCall above, this cover
          // is a little more convenient to use.  The arguments to the
-         // builtin should be given as arg1 and arg2, NULL is used if 
+         // builtin should be given as arg1 and arg2, NULL is used if
          // fewer than 2 args to pass. The method returns a Location
          // for the new temp var holding the result.  For those
          // built-ins with no return value (Print/Halt), no temporary
          // is created and NULL is returned.
     Location *GenBuiltInCall(BuiltIn b, Location *arg1 = NULL, Location *arg2 = NULL);
-
-    
+ 
          // These methods generate the Tac instructions for various
          // control flow (branches, jumps, returns, labels)
          // One minor detail to mention is that you can pass NULL
@@ -144,22 +143,19 @@ class CodeGenerator {
     void GenGoto(const char *label);
     void GenReturn(Location *val = NULL);
     void GenLabel(const char *label);
-
-
+ 
          // These methods generate the Tac instructions that mark the start
-         // and end of a function/method definition. 
+         // and end of a function/method definition.
     BeginFunc *GenBeginFunc();
     void GenEndFunc();
-
-    
+ 
          // Generates the Tac instructions for defining vtable for a
          // The methods parameter is expected to contain the vtable
          // methods in the order they should be laid out.  The vtable
          // is tagged with a label of the class name, so when you later
          // need access to the vtable, you use LoadLabel of class name.
     void GenVTable(const char *className, List<const char*> *methodLabels);
-
-
+ 
          // Emits the final "object code" for the program by
          // translating the sequence of Tac instructions into their mips
          // equivalent and printing them out to stdout. If the debug
@@ -168,5 +164,5 @@ class CodeGenerator {
          // useful in debugging to first make sure your Tac is correct.
     void DoFinalCodeGen();
 };
-
+ 
 #endif
